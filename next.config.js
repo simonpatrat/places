26ea05c1fc6084858acfabb3acd34430140f75b9
simonpatrat <simon.patrat@gmail.com>
2020-04-26 14:39:03 -0400

    trying to add blog functionality

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const glob = require("glob");
+
 module.exports = {
   webpack: (cfg) => {
     cfg.module.rules.push({
@@ -6,5 +8,30 @@ module.exports = {
       options: { mode: ["react-component"] },
     });
     return cfg;
+  },
+  exportPathMap: async function (defaultPathMap) {
+    // get all .md files in the posts dir
+    const blogPostFiles = glob.sync("content/posts/**/*.md");
+
+    // remove path and extension to leave filename only
+    const blogPostSlugs = blogPostFiles.map((file) =>
+      file.split("/")[2].replace(/ /g, "-").slice(0, -3).trim()
+    );
+
+    const createPathObject = (pathObject, slug) => {
+      return {
+        ...pathObject,
+        [`/posts/${slug}`]: {
+          page: "/posts/[slug]",
+          query: { slug: slug },
+        },
+      };
+    };
+    const blogPostsPathMap = blogPostSlugs.reduce(createPathObject, {});
+
+    return {
+      ...defaultPathMap,
+      ...blogPostsPathMap,
+    };
   },
 };
---
+++ pages/posts/[slug].js
@@ -0,0 +1,40 @@
+const Post = (props) => {
+  if (!props.post) {
+    return <div>Loading...</div>;
+  }
+  const {
+    post: {
+      attributes: { title, thumbnail, rating, date, body },
+      html,
+    },
+  } = props;
+
+  return (
+    <>
+      <article>
+        <h1>{title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: html }} />
+        <div>{date}</div>
+        <div>{rating}</div>
+        <div>{body}</div>
+        <div>{thumbnail}</div>
+      </article>
+    </>
+  );
+};
+
+Post.getInitialProps = async (ctx) => {
+  const {
+    query: { slug },
+  } = ctx;
+
+  console.log({ slug });
+
+  if (slug) {
+    const post = await import(`../../content/posts/${slug}.md`);
+    return { post };
+  }
+  return {};
+};
+
+export default Post;
