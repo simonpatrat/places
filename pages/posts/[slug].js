dcce32fdbd7ad21b2ed486c9fdf8e159ffc7bdf1
simonpatrat <simon.patrat@gmail.com>
2020-04-28 13:35:55 -0400

    Add colorThief package to get images color palettes Adapt Post template code to use react state  and colorThief to set post image color pallete

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,13 +1,37 @@
+import React, { useState, useEffect, useRef, useCallback } from "react";
+import ColorThief from "colorthief";
+
 const Post = (props) => {
+  const [postImageColorPalette, setPostImageColorPalette] = useState(null);
+  const [postImageLoaded, setPostImageLoaded] = useState(false);
+
   if (!props.post) {
     return <div>Loading...</div>;
   }
+
   const {
     post: {
       attributes: { title, featuredImage, rating, date, resume },
       html,
     },
   } = props;
+
+  const postImage = useRef();
+  useEffect(() => {
+    // code to run on component mount
+    const colorThief = new ColorThief();
+    let img = new Image();
+    img.crossOrigin = "Anonymous";
+
+    img.addEventListener("load", function () {
+      const palette = colorThief.getPalette(img, 5);
+      setPostImageColorPalette(palette);
+      setPostImageLoaded(true);
+    });
+
+    img.crossOrigin = "Anonymous";
+    img.src = featuredImage;
+  }, []);
 
   return (
     <>
@@ -18,7 +42,36 @@ const Post = (props) => {
         <div>{resume}</div>
         <div>{rating}</div>
         <div>
-          <img src={featuredImage} alt={title} />
+          {postImageLoaded ? (
+            <img
+              ref={postImage}
+              src={featuredImage}
+              alt={title}
+              crossOrigin="anonymous"
+              style={{
+                width: "600px",
+                maxWidth: `100%`,
+                height: `auto`,
+              }}
+            />
+          ) : (
+            <div>Loading image...</div>
+          )}
+
+          {!!postImageColorPalette &&
+            postImageColorPalette.map((color, index) => {
+              return (
+                <div
+                  key={index}
+                  style={{
+                    width: "20px",
+                    height: "20px",
+                    padding: "10px",
+                    background: `rgba(${color.join(",")})`,
+                  }}
+                ></div>
+              );
+            })}
         </div>
       </article>
     </>
